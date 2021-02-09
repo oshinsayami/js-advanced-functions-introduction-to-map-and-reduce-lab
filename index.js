7a37f8e152156cf2f112a3c7d9919b21b9135d49
Simran <mdrsimran@gmail.com>
2021-02-09 16:49:08 -0600

    commit tree f6353a9bc749ba2c4aa4f2f97d03c5c404c80b27

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Your code here
+const mapToNegativize = function(array){
+    let b=[]
+    array.map(a=>{
+        b.push(a*(-1))
+    })
+    return b;
+}
+const mapToNoChange = function (array) {
+    const arr=[...array]
+    return arr;
+}
+
+const mapToDouble = function (array){
+    let b=[]
+    array.map(a=>{
+        b.push(a*2)
+    })
+    return b;
+}
+
+const mapToSquare = function (array){
+    let b=[]
+    array.map(a=>{
+        b.push(a**2)
+    })
+    return b;
+}
+
+const reduceToTotal= function (sourceArray, startingPoint=0){
+    let total = startingPoint
+    for(let i=0; i<sourceArray.length; i++){
+        total += sourceArray[i];
+    }
+    return total;
+}
+
+const reduceToAllTrue = function(sourceArray){
+    for (let i = 0; i < sourceArray.length; i++ ) {
+        if (!sourceArray[i]) return false
+      }
+      return true
+    }
+    
+function reduceToAnyTrue(sourceArray) {
+      for (let i = 0; i < sourceArray.length; i++ ) {
+        if (sourceArray[i]) return true
+      }
+      return false
+}
